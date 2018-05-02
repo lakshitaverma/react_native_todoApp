5d548b1393099e33148a8aa3f0061ecbbe15dd99
linux08 <abimbola130@gmail.com>
2018-05-02 21:39:56 +0100

    Modified app.json

--- App.js
+++ App.js
@@ -1,28 +1,43 @@
 import React from 'react';
-import { StyleSheet, View, Button, TouchableOpacity, Dimensions, ScrollView, Animated, AlertIOS, FlatList } from 'react-native';
-import { Container, Header, Title, Content, Icon, button, Card, CardItem, Text, Body, Left, Right, IconNB, Footer } from "native-base";
+import Expo from 'expo';
+import { StyleSheet, View, Button, TouchableOpacity, Dimensions, ScrollView, Animated, AlertIOS, FlatList, Platform } from 'react-native';
+import { Container, Header, Title, Content, Icon, button, Card, CardItem, Text, Body, Left, Right, IconNB, Footer, } from "native-base";
 import { MaterialIcons, MaterialCommunityIcons, FontAwesome } from '@expo/vector-icons'
 import moment from 'moment';
+import prompt from 'react-native-prompt-android';
+
 
 const SCREEN_HEIGHT = Dimensions.get('window').height;
 
 
-
 export default class Todo extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      text: '',
+      data: [
+        'ade', 'bolu', 'shade'
+      ],
+      loading: true,
+    }
 
-  state = {
-    text: '',
-    data: [
-      'ade', 'bolu', 'shade'
-    ]
   }
 
+  async componentWillMount() {
+    await Expo.Font.loadAsync({
+    Roboto: require("native-base/Fonts/Roboto.ttf"),
+    Roboto_medium: require("native-base/Fonts/Roboto_medium.ttf"),
+    Ionicons: require("@expo/vector-icons/fonts/Ionicons.ttf")
+    });
+    this.setState({ loading: false });
+    }
+
   add = (text) => {
-    
+
     let notEmpty = text.trim().length > 0;
 
     if (notEmpty) {
-     
+
       this.setState(
         prevState => {
           let { data } = prevState;
@@ -36,44 +51,59 @@ export default class Todo extends React.Component {
   }
 
   remove = (i) => {
-  
+
     this.setState(
       prevState => {
         let data = prevState.data.slice();
-
         data.splice(i, 1);
-
         return { data };
       }
     );
 
   }
 
-  // updateState = 
+  // updateState
   showForm = () => {
+    if (Platform.OS == 'android') {
+      prompt(
+        'Enter Text',
+        [
+          { text: 'Cancel', onPress: () => console.log('Cancel Pressed'), style: 'cancel' },
+          { text: 'OK', onPress: text => this.add(text) },
+        ],
+        {
+          type: 'secure-text',
+          cancelable: false,
+          defaultValue: 'test',
+          placeholder: 'placeholder'
+        }
+      );
+    }
 
-    AlertIOS.prompt(
-      'Enter  Text',
-      null,
-      text =>  this.add(text)
-    );
-  
+    else {
+      AlertIOS.prompt(
+        'Enter  Text',
+        null,
+        text => this.add(text)
+      );
+    }
+
   }
 
 
-
   render() {
+    
+    if (!!this.state.loading) {
+    return (<Expo.AppLoading />);
+    }
 
     return (
-
       <Container style={{ flex: 1 }}>
         <Header>
           <Left>
             <Title>{moment().format('MMMM Do YYYY')}</Title>
           </Left>
-          <Body>
-
-          </Body>
+          <Body/>
           <Right>
             <Body>
               <Title>{moment().format('dddd')}</Title>
@@ -85,10 +115,8 @@ export default class Todo extends React.Component {
           <FlatList
             data={this.state.data}
             renderItem={({ item, index }) =>
-
-
               <Card key={index}>
-                <CardItem  key={index} style={{ height: 50 }}>
+                <CardItem key={index} style={{ height: 50 }}>
                   <Body>
                     <Text >
                       {item}
@@ -96,7 +124,7 @@ export default class Todo extends React.Component {
                   </Body>
                   <Right>
                     <TouchableOpacity style={{ alignItems: 'center', justifyContent: 'center', padding: 5, borderRadius: 5, borderColor: '#32CD32' }}
-                    onPress={() => this.remove(index)}
+                      onPress={() => this.remove(index)}
                     >
                       <FontAwesome name="minus" size={10} color='#32CD32' />
                     </TouchableOpacity >
@@ -106,10 +134,7 @@ export default class Todo extends React.Component {
 
             }
             keyExtractor={item => item.toString()}
-
           />
-
-
         </Content>
         <View style={{ alignItems: 'center', justifyContent: 'center' }} >
           <TouchableOpacity style={{ backgroundColor: '#32CD32', alignItems: 'center', justifyContent: 'center', padding: 20, borderRadius: 100 }}
@@ -118,7 +143,6 @@ export default class Todo extends React.Component {
             }>
             >
             <FontAwesome name="plus" size={20} />
-
           </TouchableOpacity>
         </View >
       </Container>
@@ -126,7 +150,6 @@ export default class Todo extends React.Component {
     );
   }
 }
-
 
 
 
